c1323f44330db984dfe227cbd96d2afd417ea350
surya pelluru <maladikrishnakishore@gmail.com>
2024-05-10 22:58:33 +0530

    terraform

---
+++ count/ec2.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "db" {
+  #count = 3
+  count = length(var.instance_names)
+  ami = "ami-090252cbe067a9e58"
+  vpc_security_group_ids = [ aws_security_group.allow-ssh.id ]
+  instance_type = "t2.micro" 
+  tags = {
+    Name = var.instance_names[count.index]
+  }
+}
+
+resource "aws_security_group" "allow-ssh" {
+    name = "allow-ssh"
+    description = "Allow ssh inbound traffic and all outbound traffic"
+
+    ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    egress {
+    from_port        = 0  ## 0 to 0 opening all protocals
+    to_port          = 0
+    protocol         = "-1" #-1 all protocals
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    tags = { 
+        Name = "allow-ssh"
+        createdby = "Krishna"
+    }
+}
---
+++ count/variables.tf
@@ -0,0 +1,4 @@
+variable "instance_names" {
+  type = list 
+  default = ["db_practice" ,"frontend_practice", "backend_practice"]  #optional
+}
