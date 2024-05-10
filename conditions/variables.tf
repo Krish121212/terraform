8fa8d85f735c41329f3fe46737a2080a9bea0309
Krish121212 <167047187+Krish121212@users.noreply.github.com>
2024-05-10 23:01:11 +0530

    Update variables.tf

--- conditions/variables.tf
+++ conditions/variables.tf
@@ -4,20 +4,6 @@ variable "image_id" {
   description = "RHEL 9 instances"  #optional
 }
 
-variable "instance_type" {
-  type = string #optional
-  default = "t2.small" 
-  description = "RHEL 9 instances"  #optional
-}
-
-variable "tags_Expense" {
-    default =  {
-        project = "Expense"
-        Name = "db"
-        Environment = "dev"
-    }  
-}
-
 variable "instance_name" {
   default = "frontend"
 }
